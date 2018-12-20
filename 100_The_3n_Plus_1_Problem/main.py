6073fabc5bc270d436f74bc7c1a4d9a186cfefd7
Jason Huh <jasonhuh@hotmail.com>
2018-12-20 11:56:59 -0800

    Update main.py

--- 100_The_3n_Plus_1_Problem/main.py
+++ 100_The_3n_Plus_1_Problem/main.py
@@ -1,6 +1,3 @@
-#Note: Work in progress
-import sys
-
 def cycle(memo, n):
     if n in memo:
         return memo[n]
@@ -9,21 +6,21 @@ def cycle(memo, n):
     memo[n] = 1 + cycle(memo, n//2 if n % 2 == 0 else 3*n + 1)
     return memo[n]
 
-def solve(i, j):
+def solve(memo, i, j):
     if i > j:
         i, j = j, i
-    memo, res = {}, 0
+    res = 0
     for n in range(i, j + 1):
         res = max(res, cycle(memo, n))
     return res
 
 if __name__ == '__main__':
-
-    for line in sys.stdin:
-        i, j = map(int, input().rsplit())
-        ans = solve(i, j)
+    memo = {}
+    while True:
+        try:
+            i, j = map(int, input().split())
+        except EOFError:
+            break
+            
+        ans = solve(memo, i, j)
         print('{} {} {}'.format(i, j, ans))
-    
-    exit(0)
-
-#         # i, j = map(int, line.split()[:2])

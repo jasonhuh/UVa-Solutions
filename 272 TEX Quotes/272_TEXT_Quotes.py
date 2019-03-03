8a9441eb322ec7869acc8106af9d810995915cf2
Jason Huh <hankey0@gmail.com>
2019-03-03 06:33:05 -0800

    Update 272_TEXT_Quotes.py

--- 272 TEX Quotes/272_TEXT_Quotes.py
+++ 272 TEX Quotes/272_TEXT_Quotes.py
@@ -1,9 +1,15 @@
+cnt_double_quote = 0
+
 def solve(s):
-    res, first = '', True
+    global cnt_double_quote
+    res = ''
     for c in s:
         if c == '"':
-            res += '``' if first else "''"
-            first = False if first else True
+            cnt_double_quote += 1
+            if cnt_double_quote % 2 == 1: # First quote
+                res += '``'
+            else: # Second quote
+                res += "''"
         else:
             res += c    
     return res
@@ -11,6 +17,8 @@ def solve(s):
 def test_simple():
     assert solve('"To be or not to be," quoth the bard, "that is the question."') \
             == "``To be or not to be,'' quoth the bard, ``that is the question.''"
+    assert solve('The programming contestant replied: "I must disagree.') \
+            == "The programming contestant replied: ``I must disagree."
 
 if __name__ == '__main__':
     while True:
